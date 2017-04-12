10161f76760d6cff084e04796c807fa0fecca0ba
Jean Detoeuf <thebignet@gmail.com>
2017-04-12 19:31:26 +0200

    Initial from spring.io

---
+++ src/main/java/edu/lyon1/Tp2Application.java
@@ -0,0 +1,12 @@
+package edu.lyon1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp2Application.class, args);
+	}
+}
