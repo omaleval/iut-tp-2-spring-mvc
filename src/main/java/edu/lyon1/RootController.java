7f2272d19f0c16cfe5b4b5b0c0c26255225d1520
Jean Detoeuf <thebignet@gmail.com>
2017-04-29 18:58:42 +0200

    test etape 1

--- src/main/java/edu/lyon1/RootController.java
+++ src/main/java/edu/lyon1/RootController.java
@@ -1,7 +1,13 @@
 package edu.lyon1;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -9,11 +15,20 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/")
 public class RootController {
 
+  public List<String> list = new ArrayList<>();
+
   @RequestMapping("/")
-  public ModelAndView test(HttpServletRequest request) {
+  public ModelAndView test(@RequestHeader HttpHeaders headers) {
+
+    Map map = HttpHeaders.getParameterMap();
+
+    //list = Collections.list(headers);
+
     ModelAndView mav = new ModelAndView();
     mav.addObject("titre", "titre");
     mav.addObject("corps", "corps");
+    mav.addObject("liste", list);
+
     mav.setViewName("template");
     return mav;
   }
