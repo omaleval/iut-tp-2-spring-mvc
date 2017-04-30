974afbe3abc715e5b70f20c6396996139188d6f6
Jean Detoeuf <thebignet@gmail.com>
2017-04-30 18:54:31 +0200

    correction de l'étape 6

--- src/main/java/edu/lyon1/RootController.java
+++ src/main/java/edu/lyon1/RootController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -60,8 +61,8 @@ public class RootController {
 
   @ResponseBody
   @RequestMapping(value = "/user", method = RequestMethod.GET)
-  public String user() {
-    return "Moi";
+  public String user(@RequestParam("nom")String nom, @RequestParam("prenom") String prenom) {
+    return prenom+" "+nom;
   }
 
 }
